fc9aa824fb1f2d7cdc03b08eb427acde6ab8ce90
Phil Kedy <phil.kedy@gmail.com>
2022-08-12 15:50:55 -0400

    Fix diffcheck.sh

--- testdata/diffcheck.sh
+++ testdata/diffcheck.sh
@@ -5,17 +5,22 @@ set -e
 FAILED=false
 
 for dir in */; do
-    echo "Checking $dir for diffs"
-    (
-        cd "$dir"
-        if ! diff -r ./expected ./actual ; then
-            FAILED=true
-        fi
-    )
+    if ! (
+        FAILED=true
+        echo "Generating code in $dir"
+        mkdir -p "$dir/actual"
+        cd "$dir/actual"
+        apex generate ../apex.yaml
+        cd ..
+        echo "Checking $dir for diffs"
+        diff -r ./expected ./actual
+    ) ; then
+        FAILED=true
+    fi
     echo "------------------------------------"
 done
 
-if [ FAILED ] ; then
+if [ "$FAILED" = true ] ; then
     echo "Exiting with code -1 due to differences"
     exit -1
 fi
