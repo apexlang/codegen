61f20ebe9ec59e1b674837e3d1e2569e25f9a107
Jarrod Overson <jsoverson@gmail.com>
2023-01-05 11:07:56 -0500

    added typescript plugin

---
+++ src/typescript/plugin.ts
@@ -0,0 +1,34 @@
+import { Configuration } from 'https://deno.land/x/apex_cli@v0.0.6/src/config.ts';
+import * as apex from 'https://deno.land/x/apex_core@v0.1.0/mod.ts';
+
+const importUrl = new URL('.', import.meta.url);
+
+function urlify(relpath: string): string {
+  const url = new URL(relpath, importUrl).toString();
+  console.error(url);
+  return url;
+}
+
+export default function (
+  doc: apex.ast.Document,
+  config: Configuration
+): Configuration {
+  config.generates ||= {};
+
+  config.config ||= {};
+  config.config.aliases ||= {};
+  (config.config.aliases as any).UUID = {
+    type: true,
+    import: 'v4 as uuidv4',
+    from: 'uuid',
+    default: 'uuidv4()',
+  };
+  config.generates[`./src/interfaces.ts`] = {
+    ifNotExists: true,
+    module: 'https://deno.land/x/apex_codegen/typescript/mod.ts',
+    visitorClass: 'InterfacesVisitor',
+    config: {},
+  };
+
+  return config;
+}
