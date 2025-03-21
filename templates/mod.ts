7ea8cd1d31742b5aeea0bdfed121c4bc8b3ed8a9
Phil Kedy <phil.kedy@gmail.com>
2025-03-21 09:12:28 -0400

    Add definitions to top-level module

--- templates/mod.ts
+++ templates/mod.ts
@@ -1,4 +1,5 @@
-import { Template } from "../deps/@apexlang/apex/config/mod.ts";
+// deno-lint-ignore-file require-await
+import { FSStructure, Template } from "../deps/@apexlang/apex/config/mod.ts";
 
 const template: Template = {
   info: {
@@ -13,6 +14,17 @@ const template: Template = {
     "nodejs/mod.ts",
     "python/mod.ts",
   ],
+
+  // deno-lint-ignore no-explicit-any
+  async process(_vars: any): Promise<FSStructure> {
+    return {
+      definitions: {
+        "@apexlang/core": "../definitions/core/index.apex",
+        "@apexlang/openapi": "../definitions/openapi/index.apex",
+        "@apexlang/rest": "../definitions/rest/index.apex",
+      },
+    };
+  },
 };
 
 export default template;
